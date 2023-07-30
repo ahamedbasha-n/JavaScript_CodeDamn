164e98b6cb730b081892cc33ba0c6ce2a9c7ac31
Ahamed Basha N <69695103+ahamedbasha-n@users.noreply.github.com>
2023-07-30 15:04:03 +0530

    Day 2 Uploaded

---
+++ Day_2/datatype.js
@@ -0,0 +1,10 @@
+// Create a variable `learningPlatform` and set it to "codedamn"
+// Create a variable `myAge`. This variable should be a number
+// Create a variable called `amIAbove18`. This variable should have either true or false as the value
+
+const learningPlatform = 'codedamn'
+const myAge = 22
+const amIAbove18 = true
+console.log(learningPlatform)
+console.log(myAge)
+console.log(amIAbove18)
---
+++ Day_2/type_of.js
@@ -0,0 +1,11 @@
+const learningPlatform = 'codedamn'
+const myAge = 22
+const amIAbove18 = true
+console.log(learningPlatform)
+console.log(myAge)
+console.log(amIAbove18)
+
+const typeOfNumber = (typeof myAge)
+const typeOfString = (typeof learningPlatform)
+console.log(typeOfNumber)
+console.log(typeOfString)
